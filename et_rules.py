e57faeaf0c2a56626ce62aeff134f85b838f7743
devdg <me@davidguest.uk>
2021-12-22 10:52:57 +0000

    added search all feature

--- et_rules.py
+++ et_rules.py
@@ -4,6 +4,12 @@ import pandas as pd
 from datetime import datetime
 import requests
 import re
+import argparse
+
+parse = argparse.ArgumentParser()
+parse.add_argument("-a","--all", help="Shows all new additions rather than the standard set", action="store_true")
+parse.add_argument("-s","--search", type=str, help="When you need to find something specific")
+args=parse.parse_args()
 
 baseurl=r'https://rules.emergingthreats.net/open/snort-2.9.0/rules/'
 fixed_files=["emerging-attack_response.rules","emerging-exploit.rules","emerging-malware.rules","emerging-policy.rules"]
@@ -21,8 +27,15 @@ def get_ets():
 	tables=pd.read_html(baseurl)
 	table=tables[0]
 	last_update=table.loc[table["Name"] == "LICENSE"]["Last Modified"].values[0]
-	sig_date=datetime.strptime(last_update,"%Y-%m-%dT%H:%M:%SZ").strftime("%Y_%m_%d")
 	files=table[table["Name"].str.contains("rules")]["Name"].values
-	get_content(fixed_files,sig_date)
+	seek_for=datetime.strptime(last_update,"%Y-%m-%dT%H:%M:%SZ").strftime("%Y_%m_%d")
+
+	if args.search:
+		seek_for=args.search
+
+	if not args.all:
+		files=fixed_files
+
+	get_content(files,seek_for)
 
 get_ets()
