409e82b92f2670e08abf05b7e7096fd6347ed801
David <96266920+devdg@users.noreply.github.com>
2021-12-22 00:39:00 +0000

    Add files via upload

---
+++ et_rules.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import numpy
+import webbrowser
+import pandas as pd
+from datetime import datetime
+import os
+import requests
+import re
+
+baseurl=r'https://rules.emergingthreats.net/open/snort-2.9.0/rules/'
+fixed_files=["emerging-attack_response.rules","emerging-exploit.rules","emerging-malware.rules","emerging-policy.rules"]
+
+def get_content(files,sig_date):
+	for file in files:
+		url=baseurl+file
+		out=requests.get(url)
+		result=re.findall(f'.*{sig_date}.*', out.text)
+		if result:
+			print(f'Matches in {file}:')
+			for results in result:
+				print(results)
+def get_ets():
+	tables=pd.read_html(baseurl)
+	table=tables[0]
+	last_update=table.loc[table["Name"] == "LICENSE"]["Last Modified"].values[0]
+	sig_date=datetime.strptime(last_update,"%Y-%m-%dT%H:%M:%SZ").strftime("%Y_%m_%d")
+	files=table[table["Name"].str.contains("rules")]["Name"].values
+	get_content(fixed_files,sig_date)
+
+get_ets()
